880e0c04e8da97059d97f4ac50f03b78f642a67a
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-04-30 15:22:50 +0200

    dodanie Rwda dla mobile

--- development/js/app.js
+++ development/js/app.js
@@ -1,89 +1,115 @@
-new hoverEffect({
-  parent: document.querySelector(".distortion"),
-  intensity: 0.2,
-  image1: "./images/01.png",
-  image2: "./images/02.png",
-  displacementImage: "./images/diss.png",
-  imagesRatio: 380 / 300
-});
+document.addEventListener("DOMContentLoaded", () => {
+  new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./images/01.png",
+    image2: "./images/02.png",
+    displacementImage: "./images/diss.png",
+    imagesRatio: 380 / 300
+  });
 
+  // NAVBAR
 
-// NAVBAR
+  gsap.from(".navbar div", {
+    duration: 1.5,
+    delay: 1.5,
+    opacity: 0,
+    y: "20",
+    ease: "expo.inOut",
+    stagger: 0.08
+  });
 
-gsap.from(".navbar div", {
-  duration: 1.5,
-  delay: 1.5,
-  opacity: 0,
-  y: "20",
-  ease: "expo.inOut",
-  stagger: 0.08
-});
+  // MEADIA
 
+  gsap.from(".media ul li", {
+    duration: 1.5,
+    delay: 1.5,
+    opacity: 0,
+    x: "-20",
+    ease: "expo.inOut",
+    stagger: 0.08
+  });
 
-// MEADIA
+  // TEXT
 
-gsap.from(".media ul li", {
-  duration: 1.5,
-  delay: 1.5,
-  opacity: 0,
-  x: "-20",
-  ease: "expo.inOut",
-  stagger: 0.08
-});
+  gsap.from(".hidetext", {
+    duration: 1.5,
+    delay: 1,
+    y: "100%",
+    ease: "expo.inOut",
+    stagger: 0.15
+  });
 
+  gsap.from(".text h2", {
+    duration: 1.5,
+    delay: 1.6,
+    opacity: 0,
+    x: "-10000",
+    ease: "expo.inOut"
+  });
 
-// TEXT
+  //SPONSOR
+  gsap.from(".sponsor img", {
+    duration: 1.5,
+    delay: 1.6,
+    opacity: 0,
+    y: "20",
+    ease: "expo.inOut"
+  });
 
-gsap.from(".hidetext", {
-  duration: 1.5,
-  delay: 1,
-  y: "100%",
-  ease: "expo.inOut",
-  stagger: 0.15
-});
+  gsap.from(".sponsor p", {
+    duration: 1.5,
+    delay: 1.7,
+    opacity: 0,
+    y: "20",
+    ease: "expo.inOut"
+  });
 
-gsap.from(".text h2", {
-  duration: 1.5,
-  delay: 1.6,
-  opacity: 0,
-  x: "-10000",
-  ease: "expo.inOut"
-});
+  //DISTORTION
 
-//SPONSOR
-gsap.from(".sponsor img", {
-  duration: 1.5,
-  delay: 1.6,
-  opacity: 0,
-  y: "20",
-  ease: "expo.inOut"
-});
+  gsap.from(".distortion", {
+    duration: 1.5,
+    delay: 2,
+    opacity: 0,
+    y: "20",
+    ease: "expo.inOut"
+  });
 
+  //OVERLAY
 
-gsap.from(".sponsor p", {
-  duration: 1.5,
-  delay: 1.7,
-  opacity: 0,
-  y: "20",
-  ease: "expo.inOut"
-});
+  gsap.to(".overlay", {
+    duration: 1.5,
+    delay: 0.5,
+    top: "-100%",
+    ease: "expo.inOut",
+    stagger: 0.2
+  });
 
-//DISTORTION
+  // CIRCLE CURSOR
+  const mouseCursor = document.querySelector(".cursor");
+  const hoverLinks = document.querySelectorAll(".hover-link");
 
-gsap.from(".distortion", {
-  duration: 1.5,
-  delay: 2,
-  opacity: 0,
-  y: "20",
-  ease: "expo.inOut"
-});
+  window.addEventListener("mousemove", cursor);
 
-//OVERLAY
+  function cursor(e) {
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
+  }
 
-gsap.to(".overlay", {
-  duration: 1.5,
-  delay: 0.5,
-  top: "-100%",
-  ease: "expo.inOut",
-  stagger: 0.2
+  document.addEventListener("click", () => {
+    mouseCursor.classList.add("expand");
+
+    setTimeout(() => {
+      mouseCursor.classList.remove("expand");
+    }, 500);
+  });
+
+  hoverLinks.forEach(link => {
+    link.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("link-grow");
+    });
+    link.addEventListener("mouseleave", () => {
+      mouseCursor.classList.remove("link-grow");
+    });
+  });
 });
